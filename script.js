198c74cd13f3a80dc37a0079c692bc7b2e936fde
MalikSabirov <maaliksabirov@gmail.com>
2024-10-30 16:01:38 +0500

    feat: js data-types

---
+++ script.js
@@ -0,0 +1,71 @@
+const constVar = "What is your name?";
+
+let letVar = "How are you?";
+
+letVar = "How much?";
+
+// console.log(constVar);
+// console.log(letVar);
+
+// Типы данных
+// Примитивы и Ссылочные
+
+// Примитивы
+// 1. Числа
+// 2. Cтроки
+// 3. boolean
+// 4. null
+// 5. undefined
+// 6. BigInt
+
+const numVar = 1.6;
+const strVar = "Это строка"; // "" | '' | ``
+const booleanVar = true; // true | false
+const nullVar = null;
+let undefinedVar; // const undefinedVar = undefined;
+
+// console.log(typeof numVar);
+// console.log(typeof strVar);
+// console.log(typeof booleanVar);
+// console.log(typeof nullVar);
+// console.log(typeof undefinedVar);
+
+// const button = document.getElementById("button");
+// button.addEventListener("click", () => {
+//   alert("CLICK!!!");
+// });
+
+// // Ссылочные
+// // Объекты
+// const myObj = {};
+
+// ЧИСЛА
+const a = 10.121551;
+const b = 20;
+
+const sum = a + b;
+const diff = a - b;
+const multiply = a * b;
+const divide = b / a;
+
+// console.log({
+//   a,
+//   b,
+//   sum,
+//   diff,
+//   multiply,
+//   divide,
+// });
+
+console.log(a.toFixed(2));
+console.log(a);
+
+const nameVar = "Malik";
+// СТРОКИ
+const strVar1 = "Это строка"; // "" | '' | ``
+const strVar2 = "Это строка"; // "" | '' | ``
+const strVar3 = `Это строка ${nameVar}`; // "" | '' | ``
+const strVar4 = "!Это строка " + nameVar; // "" | '' | ``
+
+// console.log('ТОО "Hello world"');
+console.log(strVar4);
